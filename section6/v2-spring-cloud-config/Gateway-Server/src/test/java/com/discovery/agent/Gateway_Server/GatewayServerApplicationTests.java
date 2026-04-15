5973814bafe0bd6bb0128c6f4da851d0e3852c1d
Pragya <pragyav180@gmail.com>
2026-04-15 23:28:02 +0530

    adding changes for api gateway project

---
+++ section6/v2-spring-cloud-config/Gateway-Server/src/test/java/com/discovery/agent/Gateway_Server/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discovery.agent.Gateway_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
